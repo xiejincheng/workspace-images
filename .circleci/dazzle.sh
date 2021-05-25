5ee96ca5189d2a381c29e9f8452c5f429203a720
Cornelius A. Ludmann <cornelius@gitpod.io>
2021-05-25 19:20:56 +0000

    Update .circleci

---
+++ .circleci/dazzle.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -xe
+
+if [ -z $DOCKER_USER ]; then
+  echo "DOCKER_USER is mandatory"
+  exit 2
+fi
+
+if [ -z $DOCKER_PASS ]; then
+  echo "DOCKER_PASS is mandatory"
+  exit 3
+fi
+
+DOCKERFILE=`basename $1`
+IMAGE_NAME=$2
+TAR_FILENAME=$PWD/$3
+BUILD_TAG="build-branch-$(echo $CIRCLE_BRANCH | sed 's_/_-_g')"
+
+# Log in to Docker Hub.
+# Use heredoc to avoid variable getting exposed in trace output.
+# Use << (<<< herestring is not available in busybox ash).
+# We'll be pushing images using docker.io/gitpod thus must login accordingly
+sudo docker login -u $DOCKER_USER --password-stdin docker.io << EOF
+$DOCKER_PASS
+EOF
+
+sudo dazzle -v $*
